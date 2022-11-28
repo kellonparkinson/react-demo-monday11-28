ba88eb666cf405a8a12118f5d8eea91e8884161a
Kellon Parkinson <kellon.digital@gmail.com>
2022-11-28 16:14:33 -0700

    initial upload for monday demo

---
+++ src/components/Form.jsx
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+
+const Form = () => {
+    const [userInput, setUserInput] = useState('')
+
+    const handleChange = (e) => setUserInput(e.target.value)
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        console.log(userInput)
+        setUserInput('')
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <h1>Add a Task</h1>
+            <input type="text" value={userInput} onChange={handleChange} />
+            <button>Add Task</button>
+        </form>
+    )
+}
+
+export default Form
