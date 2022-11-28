7ebc8351c0ec17180a16ad634cfbc5b9d2c51126
Kellon Parkinson <kellon.digital@gmail.com>
2022-11-28 16:29:38 -0700

    end of demo for monday

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from 'react'
+
 import './App.css'
 import Header from './components/Header'
 import Form from './components/Form'
 
 function App() {
+  const [taskList, setTaskList] = useState([])
+
+  const addTask = (newTask) => {
+    setTaskList([...taskList, newTask])
+  }
+
   return (
     <div className="App">
       <Header />
-      <Form />
+      <Form addTask={addTask} />
     </div>
   )
 }
